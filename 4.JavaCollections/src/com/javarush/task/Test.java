e4dc7523337e103c25c95109dc4696814eba2b93
Tikhonenkova Olga <tikhonenkova.olga@gmail.com>
2021-10-28 21:34:40 +0300

    All my tasks JavaSyntax

--- 4.JavaCollections/src/com/javarush/task/Test.java
+++ 4.JavaCollections/src/com/javarush/task/Test.java
@@ -1,17 +1,12 @@
 package com.javarush.task;
 
-import javax.mail.*;
-import javax.mail.internet.*;
-
-
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) throws IOException, MessagingException {
@@ -21,26 +16,61 @@ public class Test {
 //        Files.copy(is, new File("C:/Users/Оля/Desktop/page").toPath());
 
         //отправка письма самой себе
-        Properties properties = new Properties();
-        properties.put("mail.transport.protocol", "smtps");
-        properties.put("mail.host", "smtp.mail.ru");
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.auth", "true");
+//        Properties properties = new Properties();
+//        properties.put("mail.transport.protocol", "smtps");
+//        properties.put("mail.host", "smtp.mail.ru");
+//        properties.put("mail.smtp.ssl.enable", "true");
+//        properties.put("mail.smtp.auth", "true");
+//
+//        Session session = Session.getDefaultInstance(properties);
+//
+//        MimeMessage message = new MimeMessage(session);
+//        message.setSubject("test");
+//        message.setText("Hello baby!!!");
+//        message.addRecipient(Message.RecipientType.TO, new InternetAddress("tyryshkina@mail.ru"));
+//        message.setSentDate(new Date());
+//        message.addFrom(new Address[]{new InternetAddress("tyryshkina@mail.ru")});
+//
+//        String userLogin = "tyryshkina@mail.ru";
+//        String pass = "yjz,hm_2018";
+//        Transport transport = session.getTransport();
+//        transport.connect(userLogin, pass);
+//        transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
 
-        Session session = Session.getDefaultInstance(properties);
 
-        MimeMessage message = new MimeMessage(session);
-        message.setSubject("test");
-        message.setText("Hello baby!!!");
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress("tyryshkina@mail.ru"));
-        message.setSentDate(new Date());
-        message.addFrom(new Address[]{new InternetAddress("tyryshkina@mail.ru")});
+       // работа со stream
 
-        String userLogin = "tyryshkina@mail.ru";
-        String pass = "yjz,hm_2018";
-        Transport transport = session.getTransport();
-        transport.connect(userLogin, pass);
-        transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+        List<String> list = new ArrayList<>();
+        String str = "dsergvcdfgvx";
+        for (int i = 0; i < 10; i++){
+            list.add(str);
+            str = str.substring(1);
+        }
+
+        Stream stream = list.stream();
+        
+       // stream.filter(x -> x.toString().length() == 3).forEach(System.out ::println);
+
+       // stream.forEach(x->System.out.println(x));
+
+        stream.filter(x->x.toString().matches("cd")).forEach(System.out::println);
+
+//        String[] array = {"Java", "Ruuuuussshhh"};
+//        Stream<String> streamOfArray = Arrays.stream(array);
+//        streamOfArray.map(s->s.split("")) //Преобразование слова в массив букв
+//                .flatMap(Arrays::stream).distinct() //выравнивает каждый сгенерированный поток в один поток
+//                .collect(Collectors.toList()).forEach(System.out::println);
+
+
+        String[] array = {"Java", "Ruuuuussshhh"};
+        Stream<String> streamOfArray = Arrays.stream(array);
+        streamOfArray.map(s->s.split("")) //Преобразование слова в массив букв
+                .map(Arrays::stream).distinct() //Сделать массив в отдельный поток
+                .collect(Collectors.toList()).forEach(System.out::println);
+
+
+
+
 
 
 
